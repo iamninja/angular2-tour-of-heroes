f585f98e58f552b13c919c379afdb0a8aaae3f2f
Vagios Vlachos <bagios234@gmail.com>
2016-04-10 17:52:44 +0300

    Add styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,14 +7,8 @@ import { HeroDetailComponent }                              from './hero-detail.
 
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>{{title}}</h1>
-        <nav>
-            <a [routerLink]="['Dashboard']">Dashboard</a>
-            <a [routerLink]="['Heroes']">Heroes</a>
-        </nav>
-        <router-outlet></router-outlet>
-    `,
+    templateUrl: 'app/app.component.html',
+    styleUrls:['app/app.component.css'],
     directives: [
         ROUTER_DIRECTIVES
     ],
