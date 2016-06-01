6d89875481d6c7986376ab92ac1207c58b8d658f
Vagios Vlachos <bagios234@gmail.com>
2016-06-01 05:54:41 +0300

    Setup in-memory web api and mock data

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,17 @@
-import {bootstrap} from '@angular/platform-browser-dynamic';
-import {AppComponent} from './app.component';
+// Imports for in-memory-web-api
+import { provide }                            from '@angular/core';
+import { XHRBackend }                         from '@angular/http';
 
-bootstrap(AppComponent);
+import { InMemoryBackendService, SEED_DATA }  from 'angular2-in-memory-web-api';
+import { InMemoryDataService }                from './in-memory-data.service';
+
+// Usual imports
+import { bootstrap }                          from '@angular/platform-browser-dynamic';
+import { HTTP_PROVIDERS }                     from '@angular/http';
+import { AppComponent }                       from './app.component';
+
+bootstrap(AppComponent, [
+  HTTP_PROVIDERS,
+  provide(XHRBackend, { useClass: InMemoryBackendService }), //in-memory server
+  provide(SEED_DATA, { useClass:InMemoryDataService }) // in-memory server data
+]);
